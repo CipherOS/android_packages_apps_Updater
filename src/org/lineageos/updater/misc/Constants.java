26e12df77ad3e4f75586b0a42dbe1abba18802cd
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-12-12 21:35:22 +0530

    Utils: Pass our ziptype prop as parameter to server url
    - This is to handle and serve responses separately for vanilla and
      gapps builds.
    
    Change-Id: I553f279a821a8be2c8879f92e83d72157287e923

--- src/org/lineageos/updater/misc/Constants.java
+++ src/org/lineageos/updater/misc/Constants.java
@@ -42,6 +42,7 @@ public final class Constants {
     public static final String PROP_BUILD_VERSION = "ro.cipher.build.version";
     public static final String PROP_DEVICE = "ro.cipher.device";
     public static final String PROP_NEXT_DEVICE = "ro.updater.next_device";
+    public static final String PROP_ZIP_TYPE = "ro.cipher.ziptype";
     public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "lineage.updater.allow_downgrading";
 
     public static final String PREF_INSTALL_OLD_TIMESTAMP = "install_old_timestamp";
